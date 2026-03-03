020e86d918ef3bf370fb2f23c938947e979deaea
Jackson Walraven <jtw00038@mix.wvu.edu>
2026-03-03 17:31:30 -0500

    feat: initial Vite + React project baseline

---
+++ frontend/app/app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>CS330 Project Baseline</h1>
+      <p>Your Vite + React app is running correctly!</p>
+    </div>
+  );
+}
+
+export default App;
+
