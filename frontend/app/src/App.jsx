cbfec8fa0764ef6426eecd38bf314ef94318c4e4
Jackson Walraven <jtw00038@mix.wvu.edu>
2026-03-11 14:49:47 -0400

    login page and signup page buttons/frontend

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -3,6 +3,7 @@ import cam from './assets/cam.png';
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
     </div>
   );
