3d0f33e43f825768500147170df54df9d1607cec
Squke <luke.williams8749@gmail.com>
2026-04-15 20:01:33 -0400

    Fix navbar buttons on pair and BookMovie pages to use updated log-btn class and icons

--- frontend/app/src/pages/BookMovie.jsx
+++ frontend/app/src/pages/BookMovie.jsx
@@ -159,12 +159,36 @@ function BookMovie({ authenticated, authUser, onLogout }) {
                 <button className="temp-user-btn" onClick={() => navigate("/user") } style={{ background: 'none'}}>
                   <img src={profilePicSources[authUser?.pfp_index ?? 0]} alt="Profile" className="profile-pic" style={{ width: '40px', height: '40px', objectFit:'cover' }}></img>
                 </button>
-                <button className="login-btn" onClick={onLogout}>Logout</button>
+                <button className="log-btn" onClick={onLogout}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 25 25" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                    <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/>
+                    <polyline points="16 17 21 12 16 7"/>
+                    <line x1="21" y1="12" x2="9" y2="12"/>
+                  </svg>
+                  Logout
+                </button>
               </>
             ) : (
               <>
-                <button className="temp-user-btn" onClick={() => navigate("/signup")}>Sign Up</button>
-                <button className="login-btn" onClick={() => navigate("/login")}>Login</button>
+                <button className="log-btn" onClick={() => navigate("/signup")}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"
+                    fill="none" stroke="currentColor" strokeWidth="2"
+                    strokeLinecap="round" strokeLinejoin="round">
+                    <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"/>
+                    <circle cx="8.5" cy="7" r="4"/>
+                    <line x1="20" y1="8" x2="20" y2="14"/>
+                    <line x1="23" y1="11" x2="17" y2="11"/>
+                  </svg>
+                  Sign Up
+                </button>
+                <button className="log-btn" onClick={() => navigate("/login")}>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 25 25" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                    <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"/>
+                    <polyline points="16 17 21 12 16 7"/>
+                    <line x1="21" y1="12" x2="9" y2="12"/>
+                  </svg>
+                  Login
+                </button>
               </>
             )}
           </div>
